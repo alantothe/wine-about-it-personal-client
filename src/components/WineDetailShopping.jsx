1f1cb843ea929c8e277785116968a4a3498f4541
alantothe <104442054+alantothe@users.noreply.github.com>
2023-10-01 20:35:20 -0400

    add css to WineDetailShopping to align Counter

--- src/components/WineDetailShopping.jsx
+++ src/components/WineDetailShopping.jsx
@@ -5,35 +5,39 @@ export default function WineDetailShopping({ itemInfo }) {
   return (
     <div className="flex flex-col">
       <div
-        className="w-screen flex items-center space-x-4 p-5 bg-gray-100"
+        className="w-screen flex items-center p-5 bg-gray-100"
         style={{
           borderTop: "1px solid rgb(96, 20, 30)",
           borderBottom: "1px solid rgb(96, 20, 30)",
           marginTop: "-1px",
         }}
       >
-        <img
-          className="w-32 h-32 object-contain"
-          src={itemInfo.img}
-          alt={itemInfo.WineName}
-        />
-        <div className="flex flex-col justify-between">
-          <h3
-            onClick={() => navigate(`/search/wine-detail/${itemInfo._id}`)}
-            className="text-2xl font-semibold cursor-pointer hover:underline"
-            style={{ color: "rgb(96, 20, 30)" }}
-          >
-            {itemInfo.WineName}
-          </h3>
-          <p style={{ color: "rgb(96, 20, 30)" }}>${itemInfo.Price} each</p>
-          <p style={{ color: "rgb(96, 20, 30)" }}>
-            Quantity: {itemInfo.quantity}
-          </p>
-          <p className="text-green-500 font-semibold">
-            Item Total: ${Math.floor(itemInfo.Price * itemInfo.quantity)}
-          </p>
+        <div className="w-1/2 flex items-center">
+          <img
+            className="w-32 h-32 object-contain"
+            src={itemInfo.img}
+            alt={itemInfo.WineName}
+          />
+          <div className="flex flex-col justify-between ml-4">
+            <h3
+              onClick={() => navigate(`/search/wine-detail/${itemInfo._id}`)}
+              className="text-2xl font-semibold cursor-pointer hover:underline"
+              style={{ color: "rgb(96, 20, 30)" }}
+            >
+              {itemInfo.WineName}
+            </h3>
+            <p style={{ color: "rgb(96, 20, 30)" }}>${itemInfo.Price} each</p>
+            <p style={{ color: "rgb(96, 20, 30)" }}>
+              Quantity: {itemInfo.quantity}
+            </p>
+            <p className="text-green-500 font-semibold">
+              Item Total: ${Math.floor(itemInfo.Price * itemInfo.quantity)}
+            </p>
+          </div>
         </div>
-        <Counter />
+        <div className="w-1/2 flex justify-end items-center mr-5">
+          <Counter />
+        </div>
       </div>
     </div>
   );
