77e4cd052763d1fa73cce3a5ddf1c1182b00e7a3
alantothe <104442054+alantothe@users.noreply.github.com>
2023-10-02 21:43:51 -0400

    navigate user from account page if not logged in

--- src/pages/AccountInfoPage.jsx
+++ src/pages/AccountInfoPage.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import OrderHistory from "../components/accountInfo/OrderHistory";
 import Favorites from "../components/accountInfo/Favorites";
 import UserInfoCard from "../components/accountInfo/UserInfo";
 
 export default function AccountInfoPage({ user }) {
+  const navigate = useNavigate();
+
+  console.log("User in useEffect", user);
+  if (!user) {
+    navigate("/sign-in");
+  }
+
   return (
     <div className="flex pb-5 " style={{ minHeight: "68vh" }}>
       <div className="w-1/3">
