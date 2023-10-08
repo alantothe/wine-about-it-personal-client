28d1d45c91b500082c0c3c8b8fe7440532132a07
alantothe <104442054+alantothe@users.noreply.github.com>
2023-10-08 13:07:34 -0400

    adjust reactiveness in PromoBanner and revert HeroCarousel

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -29,44 +29,15 @@ export default function HeroCarousel() {
         </div>
       )}
     >
-      <figure className="relative w-full">
-        <img
-          src="https://i.imgur.com/KfnKjz8.jpg"
-          alt="Wine glasses"
-          className="relative w-screen object-cover object-bottom"
-          style={{
-            height: "70vh",
-            fontFamily: "'HelpUsGiambattista', sans-serif",
-          }}
-        />
-        <figcaption
-          className={`flex-col mr-16 mb-40 absolute bottom-0 right-0 w-1/2 h-80 border border-white bg-white py-2 px-4 shadow-lg shadow-black/5 saturate-200 backdrop-blur-sm transition-opacity duration-500 ${
-            fadeIn ? "opacity-100" : "opacity-0"
-          }`}
-        >
-          {" "}
-          <div className="ml-5">
-            <Typography
-              style={{ fontFamily: "'HelpUsGiambattista', sans-serif" }}
-              className="mb-2 mt-2 text-4xl sm:text-2xl md:text-4xl lg:text-5xl xl:text-5xl "
-            >
-              We Have Over
-            </Typography>
-            <Typography
-              style={{ fontFamily: "'HelpUsGiambattista', sans-serif" }}
-              className="mt-1 py-4 mb-1 text-7xl xs:text-sm sm:text-sm md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-6xl"
-            >
-              200 Selections
-            </Typography>
-          </div>
-          <button
-            type="button"
-            class="ml-5 mt-6 w-1/3 text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
-          >
-            View Products
-          </button>
-        </figcaption>
-      </figure>
+      <img
+        src="https://i.imgur.com/KfnKjz8.jpg"
+        alt="Wine glasses"
+        className="w-screen object-cover"
+        style={{
+          height: "70vh",
+          fontFamily: "'HelpUsGiambattista', sans-serif",
+        }}
+      />
 
       <img
         src="https://i.imgur.com/vKJ5d5K.jpg"
--- src/components/PromoBanner.jsx
+++ src/components/PromoBanner.jsx
@@ -4,21 +4,21 @@ export default function PromoBanner() {
   return (
     <div className="flex bg-black text-white h-[42px]">
       <Typography
-        className="flex items-center justify-center text-lg w-1/3 border-r"
+        className="flex items-center text-center justify-center text-xs sm:text-small md:text-lg w-1/3 border-r"
         style={{ fontFamily: "'HelpUsGiambattista', sans-serif" }}
       >
         Free shipping over $100
       </Typography>
 
       <Typography
-        className="flex items-center justify-center text-lg w-1/3 border-r"
+        className="flex items-center text-center justify-center text-xs sm:text-small md:text-lg w-1/3 border-r"
         style={{ fontFamily: "'HelpUsGiambattista', sans-serif" }}
       >
         Over 150 Wines in our Collection
       </Typography>
 
       <Typography
-        className="flex items-center justify-center text-lg w-1/3"
+        className="flex items-center text-center justify-center text-xs sm:text-small md:text-lg w-1/3"
         style={{ fontFamily: "'HelpUsGiambattista', sans-serif" }}
       >
         24/7 Customer Support
